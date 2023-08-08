61f2159e95c6b85313113cde4fdd8ac73cdc9e2f
calangthang1406 <90557694+calangthang1406@users.noreply.github.com>
2023-08-08 09:42:46 +0700

    observer

---
+++ HotelManage/src/Model/Publisher.java
@@ -0,0 +1,14 @@
+package Model;
+
+import java.util.LinkedList;
+import java.util.List;
+import Model.HotelModel;
+
+public class Publisher {
+    private List<Subscriber> subscribers = new LinkedList<Subscriber>();
+	public void subscribe(Subscriber s) { subscribers.add(s); }
+	public void unsubscribe(Subscriber s) { subscribers.remove(s); }
+	public void notifySubscribers() {
+		for(Subscriber s: subscribers) s.update();
+	}
+}
